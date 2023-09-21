ba153985f883769983708c92d8d0cb6be79202f0
kafkastream <145674933+kafkastream@users.noreply.github.com>
2023-09-21 16:13:46 +0200

    Update streamlit_app.py
    Carga de fichero AWS con Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title("Preparando el examen Snowflake")
 streamlit.title("1.- Arquitectura de Snowflake")
 streamlit.title("2.- Usuarios, roles y permisos")
 streamlit.title("3.- Carga de datos / Snowpipe")
 streamlit.title("4.- Datos semi-estructurados")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
