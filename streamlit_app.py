bf6379aabd187ad010f5d9de45311822e1f7ae37
kafkastream <145674933+kafkastream@users.noreply.github.com>
2023-09-21 16:06:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("Preparando el examen Snowflake")
+streamlit.title("1.- Arquitectura de Snowflake")
+streamlit.title("2.- Usuarios, roles y permisos")
+streamlit.title("3.- Carga de datos / Snowpipe")
+streamlit.title("4.- Datos semi-estructurados")
